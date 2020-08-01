a56b0f3643f15a373d084d938b1989b97d3d4158
Ariazm0 <1106030429@qq.com>
2020-08-01 21:17:51 +0800

    Initial commit

---
+++ src/dao/UserDao.java
@@ -0,0 +1,87 @@
+package dao;
+
+import entity.User;
+import util.JDBCUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: Ariazm
+ * Date: 2020-07-24
+ * Time: 22:57
+ */
+public class UserDao {
+    //登录
+    public User login(User loginUser) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet set = null;
+        User user = null;
+        try {
+            connection = JDBCUtil.getConnection();
+            String sql = "select * from user where username = ? and password = ?";
+            statement = connection.prepareStatement(sql);
+            statement.setString(1,loginUser.getUserName());
+            statement.setString(2,loginUser.getPassword());
+            set = statement.executeQuery();
+            if (set.next()) {
+                user = new User();
+                user.setId(set.getInt("id"));
+                user.setUserName(set.getString("username"));
+                user.setPassword(set.getString("password"));
+                user.setAge(set.getInt("age"));
+                user.setGender(set.getString("gender"));
+                user.setEmail(set.getString("email"));
+            }
+
+
+        }catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }finally {
+            JDBCUtil.close(connection,statement,set);
+        }
+        return user;
+    }
+    //注册
+    public void register (User user) {
+        Connection connection = JDBCUtil.getConnection();
+        PreparedStatement statement = null;
+        try {
+            String sql = "insert into user values (null,?,?,?,?,?) ";
+            statement = connection.prepareStatement(sql);
+            statement.setString(1,user.getUserName());
+            statement.setString(2,user.getPassword());
+            statement.setInt(3,user.getAge());
+            statement.setString(4,user.getGender());
+            statement.setString(5,user.getEmail());
+            int ret = statement.executeUpdate();
+            if (ret != 1) {
+                System.out.println("注册失败");
+                return;
+            }
+            System.out.println("注册成功");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } finally {
+            JDBCUtil.close(connection,statement,null);
+        }
+
+    }
+
+    /*public static void main(String[] args) {
+        UserDao dao = new UserDao();
+        User user = new User();
+        user.setUserName("bit");
+        user.setPassword("123");
+        User s = dao.login(user);
+        System.out.println(s);
+    }*/
+}
---
+++ src/servlet/DeleteServlet.java
@@ -0,0 +1,65 @@
+package servlet;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dao.MusicDao;
+import entity.Music;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: Ariazm
+ * Date: 2020-07-30
+ * Time: 16:03
+ */
+@WebServlet("/deleteServlet")
+public class DeleteServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setContentType("application/json; charset=utf-8");
+        String strId = req.getParameter("id");
+        System.out.println(strId);
+        int id = Integer.parseInt(strId);
+        System.out.println("id: " + id );
+        Map<String, Object> return_map = new HashMap<>();
+        try {
+            MusicDao dao = new MusicDao();
+            Music music = dao.findMusicById(id);
+            if (music == null) {
+                return;
+            }
+            if (dao.deleteMusicById(id) == 1) {
+                File file = new File("D://javacode//onlinemusic//web//" + music.getUrl()+ ".mp3");
+                System.out.println("文件是否存在：" + file.exists());
+                System.out.println("file: " + file);
+                if (file.delete()) {
+                    System.out.println("删除成功");
+                    return_map.put("msg", true);
+                }else {
+                    System.out.println("文件名： " + file.getName());
+                    System.out.println("删除失败");
+                    return_map.put("msg", false);
+                }
+
+            } else {
+                System.out.println("删除失败");
+                return_map.put("msg", false);
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(resp.getWriter(),return_map);
+    }
+}
